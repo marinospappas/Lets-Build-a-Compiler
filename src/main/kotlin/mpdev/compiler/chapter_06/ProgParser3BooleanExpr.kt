adaf9d11067133be771360fb4ed92d00c3d8aeae
marinospappas <92875023+marinospappas@users.noreply.github.com>
2021-11-08 17:50:18 +0100

    comments on grammar updated

--- src/main/kotlin/mpdev/compiler/chapter_06/ProgParser3BooleanExpr.kt
+++ src/main/kotlin/mpdev/compiler/chapter_06/ProgParser3BooleanExpr.kt
@@ -4,6 +4,12 @@ package mpdev.compiler.chapter_06
  * Program parsing - module 3
  *
  * parse a boolean expression
+ * following is the whole grammar - note how <relation> links to <expression>
+ *     which is the link between Boolean and Numerical expressions
+ * with this grammar any integer can be boolean and vice-versa
+ * any assignment will look for a boolean expression to start with
+ * and can settle for a numeric one as it goes down the grammar
+ *
  * <b-expression> ::= <b-term> [ <orop> <b-term> ] *
  * <b-term> ::= <not-factor> [ AND <not-factor> ] *
  * <not-factor> ::= [ NOT ] <b-factor>
@@ -13,6 +19,7 @@ package mpdev.compiler.chapter_06
  * <term> ::= <signed factor> [ <mulop> factor ] *
  * <signed factor>::= [ <addop> ] <factor>
  * <factor> ::= <integer> | <variable> | ( <expression> )
+ *
  */
 
 /** parse a Boolean expression */
