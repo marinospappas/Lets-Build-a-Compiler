c606113a6e4362b37acd683bf1c05f87da0616f9
marinospappas <92875023+marinospappas@users.noreply.github.com>
2021-11-29 17:05:06 +0100

    chapter 7

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Based on the original series ["Let’s Build a Compiler!"](https://compilers.iecc.com/crenshaw) by [Jack Crenshaw](https://jackcrenshaw.com/).
 
-This is an adaptation of the original series to Kotlin. At a second stage the compiler will be further adapted to produce code for the TI-59 Texas Instruments Programmable Calculator.
+This is an adaptation of the original series to Kotlin. At a second stage the compiler will be further adapted to produce code for x86-64 that the reader can execute on Linux.
 
 At the moment the repository is split based on the chapters of the tutorial and will be updated as new chapters are added.
 
---
+++ src/main/kotlin/mpdev/compiler/chapter_07_vii/LanguageTokens.kt
@@ -0,0 +1,100 @@
+package mpdev.compiler.chapter_07_vii
+
+///////////////////////////////////////////////////////////
+// define all the keywords used in our programming language
+///////////////////////////////////////////////////////////
+
+val NO_TOKEN = "No Token"
+val END_OF_INPUT = "End of Input"
+
+class Token(val value: String = NO_TOKEN,
+            val encToken: Kwd = Kwd.none,
+            val info: TokInfo = TokInfo.none,
+            val type: TokType = TokType.none)
+
+var languageTokens = mutableListOf<Token>()
+
+// our language list of keywords and operands and any other special character sequence
+enum class Kwd {
+    addOp, subOp, mulOp, divOp,
+    equalsOp,
+    leftParen, rightParen,
+    ifToken, elseToken, endifToken,
+    whileToken, endwhileToken,
+    repeatToken, untilToken,
+    forToken, toToken, endforToken,
+    breakToken,
+    identifier, number,
+    boolTrue, boolFalse,
+    startOfProgram, endOfProgram,
+    endOfInput, any, invalid, none }
+
+enum class TokType { addOps, mulOps, booleanLit,
+    endOfBlock, endOfPRogram, endOfInput, invalid, none }
+enum class TokInfo { identifier, keyword, operator, number, invalid, none }
+
+
+// initialise our lists (= define the language)
+
+fun initKeywords() {
+    languageTokens.add(
+        Token("PROGRAM",  Kwd.startOfProgram, TokInfo.keyword,      TokType.none))
+    languageTokens.add(
+        Token("ENDPROG",  Kwd.endOfProgram,   TokInfo.keyword,      TokType.endOfPRogram))
+    languageTokens.add(
+        Token("IF",       Kwd.ifToken,        TokInfo.keyword,      TokType.none))
+    languageTokens.add(
+        Token("ELSE",     Kwd.elseToken,      TokInfo.keyword,      TokType.endOfBlock))
+    languageTokens.add(
+        Token("ENDIF",    Kwd.endifToken,     TokInfo.keyword,      TokType.endOfBlock))
+    languageTokens.add(
+        Token("WHILE",    Kwd.whileToken,     TokInfo.keyword,      TokType.none))
+    languageTokens.add(
+        Token("ENDWHILE", Kwd.endwhileToken,  TokInfo.keyword,      TokType.endOfBlock))
+    languageTokens.add(
+        Token("FOR",      Kwd.forToken,       TokInfo.keyword,      TokType.none))
+    languageTokens.add(
+        Token("TO",       Kwd.toToken,        TokInfo.keyword,      TokType.none))
+    languageTokens.add(
+        Token("ENDFOR",   Kwd.endforToken,    TokInfo.keyword,      TokType.endOfBlock))
+    languageTokens.add(
+        Token("REPEAT",   Kwd.repeatToken,    TokInfo.keyword,      TokType.none))
+    languageTokens.add(
+        Token("UNTIL",    Kwd.untilToken,     TokInfo.keyword,      TokType.endOfBlock))
+    languageTokens.add(
+        Token("BREAK",    Kwd.breakToken,     TokInfo.keyword,      TokType.none))
+
+    languageTokens.add(
+        Token("TRUE",     Kwd.boolTrue,       TokInfo.keyword,      TokType.booleanLit))
+    languageTokens.add(
+        Token("FALSE",    Kwd.boolTrue,       TokInfo.keyword,      TokType.booleanLit))
+
+    languageTokens.add(
+        Token(nullChar.toString(),  Kwd.endOfInput,  TokInfo.keyword,     TokType.endOfPRogram))
+
+    languageTokens.add(
+        Token("Identifier", Kwd.identifier,     TokInfo.identifier, TokType.none))
+    languageTokens.add(
+        Token("Number",    Kwd.number,          TokInfo.number,     TokType.none))
+}
+
+///////////////////////////
+// define all the operators
+///////////////////////////
+
+fun initOperators() {
+    languageTokens.add(
+        Token("+",      Kwd.addOp,      TokInfo.operator, TokType.addOps))
+    languageTokens.add(
+        Token("-",      Kwd.subOp,      TokInfo.operator, TokType.addOps))
+    languageTokens.add(
+        Token("*",      Kwd.mulOp,      TokInfo.operator, TokType.mulOps))
+    languageTokens.add(
+        Token("/",      Kwd.divOp,      TokInfo.operator, TokType.mulOps))
+    languageTokens.add(
+        Token("=",      Kwd.equalsOp,   TokInfo.operator, TokType.mulOps))
+    languageTokens.add(
+        Token("(",      Kwd.leftParen,  TokInfo.operator, TokType.none))
+    languageTokens.add(
+        Token(")",      Kwd.rightParen, TokInfo.operator, TokType.none))
+}
