9c913d3d1e6ce0b2d3e94ae9e24e22f761db46f2
Marinos Pappas <marinos.pappas@gmal.com>
2021-12-20 18:35:43 +0100

    chapter 10

---
+++ src/main/kotlin/mpdev/compiler/chapter_10_x/ProgParser0TopLevel.kt
@@ -0,0 +1,129 @@
+package mpdev.compiler.chapter_10_x
+
+/**
+ * Program parsing - module 0
+ * Top Level - program structure
+ */
+
+// global vars
+
+// the variables space
+val variablesSpace = mutableMapOf<String,VariableDecl>()
+
+// the functions space
+val functionsSpace = mutableMapOf<String,VarType>()
+
+/////////// support for variables declaration /////////
+/** our variable types */
+enum class VarType { int }
+
+/** our variables declaration */
+class VariableDecl(var type: VarType, var initialised: Boolean = false)
+
+/** declare a variable */
+fun declareVar(name: String, initValue: String) {
+    // check for duplicate var declaration
+    if (variablesSpace[name] != null)
+        abort ("variable $name already declared")
+    variablesSpace[name] = VariableDecl(VarType.int, initValue!="")
+    code.declareInt(name, initValue)
+}
+
+/////////// support for functions declaration /////////
+/** process a function declaration */
+fun declareFun(name: String) {
+    if (functionsSpace[name] != null)
+        abort ("function $name already declared")
+    functionsSpace[name] = VarType.int
+    code.declareAsmFun(name)
+}
+
+////////////////////////////////////////////////////////////
+
+/**
+ * parse a program
+ * <program> ::= <prog header> [ <var declarations> ] [ <fun declarations> ] <main block> <prog end>
+ */
+fun parseProgram() {
+    parseProgHeader()
+    if (inp.lookahead().encToken == Kwd.varDecl)
+        parseVarDecl()
+    code.funInit()
+    if (inp.lookahead().encToken == Kwd.funDecl)
+        parseFunDecl()
+    parseMainBlock()
+    parseProgEnd()
+}
+
+/**
+ * parse program header
+ * <program header> ::= program <identifier>
+ */
+fun parseProgHeader() {
+    inp.match(Kwd.startOfProgram)
+    code.progInit(inp.match(Kwd.identifier).value)
+}
+
+/**
+ * parse variables declarations
+ * <variable declarations> ::= var <identifier> [ = <value> ] [ , <identifier> [ = <value> ] ] *
+ */
+fun parseVarDecl() {
+    while (inp.lookahead().encToken == Kwd.varDecl) {
+        do {
+            inp.match()
+            parseOneVarDecl()
+        } while (inp.lookahead().encToken == Kwd.commaToken)
+    }
+}
+
+/** parse one variable declaration */
+fun parseOneVarDecl() {
+    val varName = inp.match(Kwd.identifier).value
+    var initValue = ""
+    if (inp.lookahead().encToken == Kwd.equalsOp) {
+        inp.match()
+        initValue = inp.match(Kwd.number).value
+    }
+    declareVar(varName, initValue)
+}
+
+/**
+ * parse functions declarations
+ * <function declaration> ::= fun <identifier> ( ) <block>
+ */
+fun parseFunDecl() {
+    while (inp.lookahead().encToken == Kwd.funDecl) {
+        inp.match()
+        val funName = inp.match(Kwd.identifier).value
+        labelPrefix = funName        // set label prefix and label index
+        labelIndx = 0
+        inp.match(Kwd.leftParen)
+        inp.match(Kwd.rightParen)
+        declareFun(funName)
+        parseBlock()
+    }
+}
+
+/**
+ * parse main block
+ * <main block> ::= main <block>
+ */
+fun parseMainBlock() {
+    labelPrefix = "main"        // set label prefix and label index
+    labelIndx = 0
+    inp.match(Kwd.mainToken)
+    code.mainInit()
+    parseBlock()
+    code.mainEnd()
+}
+
+/**
+ * parse program end
+ * <program end> ::= endprogram
+ */
+fun parseProgEnd() {
+    inp.match(Kwd.endOfProgram)
+    code.outputCodeNl()
+    code.outputComment("end program")
+}
---
+++ src/main/kotlin/mpdev/compiler/chapter_10_x/ProgParser2NumericExpr.kt
@@ -0,0 +1,135 @@
+package mpdev.compiler.chapter_10_x
+
+/**
+ * Program parsing - module
+ * Numerical Expressions
+ */
+
+/**
+ * parse assignment
+ * <assignment> ::= <identifier> = <b-expression>
+ */
+fun parseAssignment() {
+    val identName: String = inp.match(Kwd.identifier).value
+    inp.match(Kwd.equalsOp)
+    parseBooleanExpression()
+    code.assignment(identName)
+}
+
+/**
+ * parse a numeric expression
+ * <expression> ::= <term> [ <addop> <term> ] *
+ */
+fun parseExpression() {
+    parseTerm()
+    while (inp.lookahead().type == TokType.addOps) {
+        code.saveAccumulator()
+        when (inp.lookahead().encToken) {
+            Kwd.addOp -> add()
+            Kwd.subOp -> subtract()
+            else -> inp.expected("add or subtract operator")
+        }
+    }
+}
+
+/**
+ * parse a term
+ * <term> ::= <signed factor> [ <mulop> <factor> ] *
+ */
+fun parseTerm() {
+    parseSignedFactor()
+    while (inp.lookahead().type == TokType.mulOps) {
+        code.saveAccumulator()
+        when (inp.lookahead().encToken) {
+            Kwd.mulOp -> multiply()
+            Kwd.divOp -> divide()
+            else -> inp.expected("multiply or divide operator")
+        }
+    }
+}
+
+/**
+ * parse a signed factor
+ * this can be only the first factor in a term
+ * <signed factor> ::= [ addop ] <factor>
+ */
+fun parseSignedFactor() {
+    if (inp.lookahead().encToken == Kwd.addOp)
+        inp.match()
+    if (inp.lookahead().encToken == Kwd.subOp) {
+        inp.match()
+        if (inp.lookahead().encToken == Kwd.number)
+            code.setAccumulator("-${inp.match(Kwd.number).value}")
+        else {
+            parseFactor()
+            code.negateAccumulator()
+        }
+    }
+    else
+        parseFactor()
+}
+
+/**
+ * parse a factor
+ * <factor> ::= ( <expression> ) | <integer> | <identifier>
+ */
+fun parseFactor() {
+    if (inp.lookahead().encToken == Kwd.leftParen) {
+        // ( Expression )
+        inp.match()
+        parseExpression()
+        inp.match(Kwd.rightParen)
+    }
+    else
+    if (inp.lookahead().encToken == Kwd.identifier)
+        // Identifier
+        parseIdentifier()
+    else
+        // Number
+        code.setAccumulator(inp.match(Kwd.number).value)
+}
+
+/**
+ * parse an identifier
+ * <identifier> ::= <variable> | <function>
+ */
+fun parseIdentifier() {
+    val identName: String = inp.match(Kwd.identifier).value
+    if (inp.lookahead().encToken == Kwd.leftParen) {
+        // function
+        inp.match()
+        inp.match(Kwd.rightParen)
+        code.callFunction(identName)
+    }
+    else
+        // variable
+        code.setAccumulatorToVar(identName)
+}
+
+/** parse an addition */
+fun add() {
+    inp.match()
+    parseTerm()
+    code.addToAccumulator()
+}
+
+/** parse a subtraction */
+fun subtract() {
+    inp.match()
+    parseTerm()
+    code.subFromAccumulator()
+}
+
+/** parse a multiplication */
+fun multiply() {
+    inp.match()
+    parseFactor()
+    code.multiplyAccumulator()
+}
+
+/** parse a division */
+fun divide() {
+    inp.match()
+    parseFactor()
+    code.divideAccumulator()
+}
